bf61c4373cce1c264f93ec1fea1a8f2597050549
Ing. Díaz Martínez Facundo <facundo_diaz_martinez@hotmail.com>
2019-08-05 10:41:16 -0300

    first commit

---
+++ lib/ruby/anmat.rb
@@ -0,0 +1,8 @@
+require "ruby/anmat/version"
+
+module Ruby
+  module Anmat
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/anmat/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Anmat
+    VERSION = "0.1.0"
+  end
+end
